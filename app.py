7b6d288ca9a488c5d211489d4e15a817e2d09e16
Agath Clafio <114557109+AgathClafio05@users.noreply.github.com>
2024-04-02 15:00:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ local_css("style/style.css")
 # ---- LOAD ASSETS ----
 lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
 img_contact_form = Image.open("images/Bankruptcy.jpg")
-img_lottie_animation = Image.open("images/Opiod stock photo.jpg")
+img_lottie_animation = Image.open("images/Opiod stock photo.png")
+img_call_on = Image.open("images/netflix.jpg")
+
 
 
 # ---- HEADER SECTION ----
@@ -96,7 +98,7 @@ with st.container():
 with st.container():
     image_column, text_column = st.columns((1, 2))
     with image_column:
-        st.image(img_contact_form)
+        st.image(img_call_on)
     with text_column:
         st.subheader("Netflix Recommendation")
         st.write(
