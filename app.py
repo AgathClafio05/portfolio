c94254688100e6c6ec3deb7e8ffd698484472cf1
Agath Clafio <114557109+AgathClafio05@users.noreply.github.com>
2024-04-02 15:05:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
+from pathlib import Path
+
 
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
